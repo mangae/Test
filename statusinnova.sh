ca625598109ed479a123cd358909bfeddab4a72d
mangae <37798589+mangae@users.noreply.github.com>
2019-03-21 11:53:26 +0100

    Create statusinnova.sh

---
+++ statusinnova.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+sleep 1
+innova1-cli -datadir=/root/.innovacore1 masternode status
+sleep 1
+innova2-cli -datadir=/root/.innovacore2 masternode status
+sleep 1
+innova3-cli -datadir=/root/.innovacore3 masternode status
+sleep 1
+innova4-cli -datadir=/root/.innovacore4 masternode status
+sleep 1
+innova5-cli -datadir=/root/.innovacore5 masternode status
+sleep 1
+innova6-cli -datadir=/root/.innovacore6 masternode status
+sleep 1
+innova7-cli -datadir=/root/.innovacore7 masternode status
+sleep 1
+innova8-cli -datadir=/root/.innovacore8 masternode status
+sleep 1
+innova9-cli -datadir=/root/.innovacore9 masternode status
+sleep 1
+innova10-cli -datadir=/root/.innovacore10 masternode status
+sleep 1
+innova11-cli -datadir=/root/.innovacore11 masternode status
+sleep 1
+innova12-cli -datadir=/root/.innovacore12 masternode status
+sleep 1
+innova13-cli -datadir=/root/.innovacore13 masternode status
+sleep 1
+innova14-cli -datadir=/root/.innovacore14 masternode status
+sleep 1
+innova15-cli -datadir=/root/.innovacore15 masternode status
+sleep 1
+innova16-cli -datadir=/root/.innovacore16 masternode status
+sleep 1
+innova17-cli -datadir=/root/.innovacore17 masternode status
+sleep 1
+innova18-cli -datadir=/root/.innovacore18 masternode status
+sleep 1
+innova19-cli -datadir=/root/.innovacore19 masternode status
+sleep 1
+innova20-cli -datadir=/root/.innovacore20 masternode status
+sleep 1
+innova21-cli -datadir=/root/.innovacore21 masternode status
+sleep 1
+innova22-cli -datadir=/root/.innovacore22 masternode status
+sleep 1
+innova23-cli -datadir=/root/.innovacore23 masternode status
+sleep 1
+innova24-cli -datadir=/root/.innovacore24 masternode status
+sleep 1
+innova25-cli -datadir=/root/.innovacore25 masternode status
