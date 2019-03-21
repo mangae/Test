77a2b946be578207e3e92d486cba863e10ddde00
mangae <37798589+mangae@users.noreply.github.com>
2019-03-21 11:33:52 +0100

    Add files via upload

---
+++ debuginnova.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+sleep 5
+innova1-cli -datadir=/root/.innovacore1 masternode debug
+sleep 5
+innova2-cli -datadir=/root/.innovacore2 masternode debug
+sleep 5
+innova3-cli -datadir=/root/.innovacore3 masternode debug
+sleep 5
+innova4-cli -datadir=/root/.innovacore4 masternode debug
+sleep 5
+innova5-cli -datadir=/root/.innovacore5 masternode debug
+sleep 5
+innova6-cli -datadir=/root/.innovacore6 masternode debug
+sleep 5
+innova7-cli -datadir=/root/.innovacore7 masternode debug
+sleep 5
+innova8-cli -datadir=/root/.innovacore8 masternode debug
+sleep 5
+innova9-cli -datadir=/root/.innovacore9 masternode debug
+sleep 5
+innova10-cli -datadir=/root/.innovacore10 masternode debug
+sleep 5
+innova11-cli -datadir=/root/.innovacore11 masternode debug
+sleep 5
+innova12-cli -datadir=/root/.innovacore12 masternode debug
+sleep 5
+innova13-cli -datadir=/root/.innovacore13 masternode debug
+sleep 5
+innova14-cli -datadir=/root/.innovacore14 masternode debug
+sleep 5
+innova15-cli -datadir=/root/.innovacore15 masternode debug
+sleep 5
+innova16-cli -datadir=/root/.innovacore16 masternode debug
+sleep 5
+innova17-cli -datadir=/root/.innovacore17 masternode debug
+sleep 5
+innova18-cli -datadir=/root/.innovacore18 masternode debug
+sleep 5
+innova19-cli -datadir=/root/.innovacore19 masternode debug
+sleep 5
+innova20-cli -datadir=/root/.innovacore20 masternode debug
+sleep 5
+innova21-cli -datadir=/root/.innovacore21 masternode debug
+sleep 5
+innova22-cli -datadir=/root/.innovacore22 masternode debug
+sleep 5
+innova23-cli -datadir=/root/.innovacore23 masternode debug
+sleep 5
+innova24-cli -datadir=/root/.innovacore24 masternode debug
+sleep 1
+innova25-cli -datadir=/root/.innovacore25 masternode debug
+
