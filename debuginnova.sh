610c7cd8b1b67d777f078179f3a69f9cbfe8944a
mangae <37798589+mangae@users.noreply.github.com>
2019-03-21 12:07:49 +0100

    Update debuginnova.sh

--- debuginnova.sh
+++ debuginnova.sh
@@ -1,52 +1,77 @@
 #!/bin/bash
 
+echo InnovaMN1
 sleep 1
 innova1-cli -datadir=/root/.innovacore1 masternode debug
+echo InnovaMN2
 sleep 1
 innova2-cli -datadir=/root/.innovacore2 masternode debug
+echo InnovaMN3
 sleep 1
 innova3-cli -datadir=/root/.innovacore3 masternode debug
+echo InnovaMN4
 sleep 1
 innova4-cli -datadir=/root/.innovacore4 masternode debug
+echo InnovaMN5
 sleep 1
 innova5-cli -datadir=/root/.innovacore5 masternode debug
+echo InnovaMN6
 sleep 1
 innova6-cli -datadir=/root/.innovacore6 masternode debug
+echo InnovaMN7
 sleep 1
 innova7-cli -datadir=/root/.innovacore7 masternode debug
+echo InnovaMN8
 sleep 1
 innova8-cli -datadir=/root/.innovacore8 masternode debug
+echo InnovaMN9
 sleep 1
 innova9-cli -datadir=/root/.innovacore9 masternode debug
+echo InnovaMN10
 sleep 1
 innova10-cli -datadir=/root/.innovacore10 masternode debug
+echo InnovaMN11
 sleep 1
 innova11-cli -datadir=/root/.innovacore11 masternode debug
+echo InnovaMN12
 sleep 1
 innova12-cli -datadir=/root/.innovacore12 masternode debug
+echo InnovaMN13
 sleep 1
 innova13-cli -datadir=/root/.innovacore13 masternode debug
+echo InnovaMN14
 sleep 1
 innova14-cli -datadir=/root/.innovacore14 masternode debug
+echo InnovaMN15
 sleep 1
 innova15-cli -datadir=/root/.innovacore15 masternode debug
+echo InnovaMN16
 sleep 1
 innova16-cli -datadir=/root/.innovacore16 masternode debug
+echo InnovaMN17
 sleep 1
 innova17-cli -datadir=/root/.innovacore17 masternode debug
+echo InnovaMN18
 sleep 1
 innova18-cli -datadir=/root/.innovacore18 masternode debug
+echo InnovaMN19
 sleep 1
 innova19-cli -datadir=/root/.innovacore19 masternode debug
+echo InnovaMN20
 sleep 1
 innova20-cli -datadir=/root/.innovacore20 masternode debug
+echo InnovaMN21
 sleep 1
 innova21-cli -datadir=/root/.innovacore21 masternode debug
+echo InnovaMN22
 sleep 1
 innova22-cli -datadir=/root/.innovacore22 masternode debug
+echo InnovaMN23
 sleep 1
 innova23-cli -datadir=/root/.innovacore23 masternode debug
+echo InnovaMN24
 sleep 1
 innova24-cli -datadir=/root/.innovacore24 masternode debug
+echo InnovaMN25
 sleep 1
 innova25-cli -datadir=/root/.innovacore25 masternode debug
