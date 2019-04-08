053ad631a60c985b262f5a6dc20c358c18138cf8
mangae <37798589+mangae@users.noreply.github.com>
2019-04-08 20:12:39 +0200

    Create debuginn.sh

---
+++ debuginn.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+sleep 1
+innova1-cli -datadir=/root/.innovacore1 masternode debug
+sleep 1
+innova2-cli -datadir=/root/.innovacore2 masternode debug
+sleep 1
+innova3-cli -datadir=/root/.innovacore3 masternode debug
+sleep 1
+innova4-cli -datadir=/root/.innovacore4 masternode debug
+sleep 1
+innova5-cli -datadir=/root/.innovacore5 masternode debug
+sleep 1
+innova6-cli -datadir=/root/.innovacore6 masternode debug
+sleep 1
+innova7-cli -datadir=/root/.innovacore7 masternode debug
+sleep 1
+innova8-cli -datadir=/root/.innovacore8 masternode debug
+sleep 1
+innova9-cli -datadir=/root/.innovacore9 masternode debug
+sleep 1
+innova10-cli -datadir=/root/.innovacore10 masternode debug
+sleep 1
+innova11-cli -datadir=/root/.innovacore11 masternode debug
+sleep 1
+innova12-cli -datadir=/root/.innovacore12 masternode debug
+sleep 1
+innova13-cli -datadir=/root/.innovacore13 masternode debug
+sleep 1
+innova14-cli -datadir=/root/.innovacore14 masternode debug
+sleep 1
+innova15-cli -datadir=/root/.innovacore15 masternode debug
+sleep 1
+innova16-cli -datadir=/root/.innovacore16 masternode debug
+sleep 1
+innova17-cli -datadir=/root/.innovacore17 masternode debug
+sleep 1
+innova18-cli -datadir=/root/.innovacore18 masternode debug
+sleep 1
+innova19-cli -datadir=/root/.innovacore19 masternode debug
+sleep 1
+innova20-cli -datadir=/root/.innovacore20 masternode debug
+sleep 1
+innova21-cli -datadir=/root/.innovacore21 masternode debug
+sleep 1
+innova22-cli -datadir=/root/.innovacore22 masternode debug
+sleep 1
+innova23-cli -datadir=/root/.innovacore23 masternode debug
+sleep 1
+innova24-cli -datadir=/root/.innovacore24 masternode debug
+sleep 1
+innova25-cli -datadir=/root/.innovacore25 masternode debug
+sleep 1
+innova26-cli -datadir=/root/.innovacore26 masternode debug
+sleep 1
+innova27-cli -datadir=/root/.innovacore27 masternode debug
+sleep 1
+innova28-cli -datadir=/root/.innovacore28 masternode debug
+sleep 1
+innova29-cli -datadir=/root/.innovacore29 masternode debug
+sleep 1
+innova30-cli -datadir=/root/.innovacore30 masternode debug
+sleep 1
+innova31-cli -datadir=/root/.innovacore31 masternode debug
+sleep 1
+innova32-cli -datadir=/root/.innovacore32 masternode debug
+sleep 1
+innova33-cli -datadir=/root/.innovacore33 masternode debug
+sleep 1
+innova34-cli -datadir=/root/.innovacore34 masternode debug
+sleep 1
+innova35-cli -datadir=/root/.innovacore35 masternode debug
+sleep 1
+innova36-cli -datadir=/root/.innovacore36 masternode debug
