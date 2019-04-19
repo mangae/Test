0b96b827eece8712650a2933c956b84eb7da7255
mangae <37798589+mangae@users.noreply.github.com>
2019-04-19 18:51:03 +0200

    Create clearlogsrv3.sh

---
+++ clearlogsrv3.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+sleep 5
+truncate -s0 ~/.ColossusXT1/debug.log
+truncate -s0 ~/.ColossusXT2/debug.log
+truncate -s0 ~/.ColossusXT3/debug.log
+truncate -s0 ~/.ColossusXT4/debug.log
+sleep 5
+truncate -s0 ~/.gobytecore1/debug.log
+truncate -s0 ~/.gobytecore2/debug.log
+truncate -s0 ~/.gobytecore3/debug.log
+truncate -s0 ~/.gobytecore4/debug.log
+truncate -s0 ~/.gobytecore5/debug.log
+truncate -s0 ~/.gobytecore6/debug.log
+truncate -s0 ~/.gobytecore7/debug.log
+truncate -s0 ~/.gobytecore8/debug.log
+truncate -s0 ~/.gobytecore9/debug.log
+truncate -s0 ~/.gobytecore10/debug.log
+truncate -s0 ~/.gobytecore11/debug.log
+truncate -s0 ~/.gobytecore12/debug.log
+truncate -s0 ~/.gobytecore13/debug.log
+truncate -s0 ~/.gobytecore14/debug.log
+truncate -s0 ~/.gobytecore15/debug.log
+truncate -s0 ~/.gobytecore16/debug.log
+truncate -s0 ~/.gobytecore17/debug.log
+truncate -s0 ~/.gobytecore18/debug.log
+truncate -s0 ~/.gobytecore19/debug.log
+truncate -s0 ~/.gobytecore20/debug.log
+sleep 5
+truncate -s0 ~/.innovacore1/debug.log
+truncate -s0 ~/.innovacore2/debug.log
+truncate -s0 ~/.innovacore3/debug.log
+truncate -s0 ~/.innovacore4/debug.log
+truncate -s0 ~/.innovacore5/debug.log
+truncate -s0 ~/.innovacore6/debug.log
+truncate -s0 ~/.innovacore7/debug.log
+truncate -s0 ~/.innovacore8/debug.log
+truncate -s0 ~/.innovacore9/debug.log
+truncate -s0 ~/.innovacore10/debug.log
+truncate -s0 ~/.innovacore11/debug.log
+truncate -s0 ~/.innovacore12/debug.log
+truncate -s0 ~/.innovacore13/debug.log
+truncate -s0 ~/.innovacore14/debug.log
+truncate -s0 ~/.innovacore15/debug.log
+truncate -s0 ~/.innovacore16/debug.log
+truncate -s0 ~/.innovacore17/debug.log
+truncate -s0 ~/.innovacore18/debug.log
+truncate -s0 ~/.innovacore19/debug.log
+truncate -s0 ~/.innovacore20/debug.log
+truncate -s0 ~/.innovacore21/debug.log
+truncate -s0 ~/.innovacore22/debug.log
+truncate -s0 ~/.innovacore23/debug.log
+truncate -s0 ~/.innovacore24/debug.log
+truncate -s0 ~/.innovacore25/debug.log
+truncate -s0 ~/.innovacore26/debug.log
+truncate -s0 ~/.innovacore31/debug.log
+truncate -s0 ~/.innovacore32/debug.log
+truncate -s0 ~/.innovacore33/debug.log
+truncate -s0 ~/.innovacore34/debug.log
+truncate -s0 ~/.innovacore35/debug.log
+truncate -s0 ~/.innovacore36/debug.log
+truncate -s0 ~/.innovacore37/debug.log
+truncate -s0 ~/.innovacore38/debug.log
+truncate -s0 ~/.innovacore39/debug.log
+truncate -s0 ~/.innovacore40/debug.log
+truncate -s0 ~/.innovacore41/debug.log
+truncate -s0 ~/.innovacore42/debug.log
+sleep 5
+truncate -s0 ~/.SagaCoin/debug.log
+truncate -s0 ~/.SagaCoin2/debug.log
+truncate -s0 ~/.SagaCoin3/debug.log
+truncate -s0 ~/.SagaCoin4/debug.log
+truncate -s0 ~/.SagaCoin5/debug.log
+truncate -s0 ~/.SagaCoin6/debug.log
+truncate -s0 ~/.SagaCoin7/debug.log
+truncate -s0 ~/.SagaCoin8/debug.log
+truncate -s0 ~/.SagaCoin9/debug.log
+truncate -s0 ~/.SagaCoin10/debug.log
+truncate -s0 ~/.SagaCoin11/debug.log
+truncate -s0 ~/.SagaCoin12/debug.log
+truncate -s0 ~/.SagaCoin13/debug.log
+truncate -s0 ~/.SagaCoin14/debug.log
+truncate -s0 ~/.SagaCoin15/debug.log
+sleep 5
+truncate -s0 ~/.mag1/debug.log
+truncate -s0 ~/.mag2/debug.log
+truncate -s0 ~/.mag3/debug.log
+truncate -s0 ~/.mag4/debug.log
+truncate -s0 ~/.mag5/debug.log
+sleep 5
+truncate -s0 ~/.syndicate1/debug.log
+truncate -s0 ~/.syndicate2/debug.log
+truncate -s0 ~/.syndicate3/debug.log
+sleep 5
+truncate -s0 ~/.escrow1/debug.log
+sleep 5
+truncate -s0 ~/..desirecore1/debug.log
+truncate -s0 ~/..desirecore2/debug.log
