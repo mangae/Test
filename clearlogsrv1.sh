db78c29b3487a7c7230c34c131dcfe71454671dd
mangae <37798589+mangae@users.noreply.github.com>
2019-09-21 11:45:51 +0200

    Update and rename clearlogsrv3.sh to clearlogsrv1.sh

---
+++ clearlogsrv1.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+
+truncate -s0 ~/.bitcoinmonstercore/debug.log
+sleep 5
+truncate -s0 ~/.bitgreen/debug.log
+truncate -s0 ~/.bitgreen_btg1/debug.log
+truncate -s0 ~/.bitgreen_btg2/debug.log
+truncate -s0 ~/.bitgreen_btg3/debug.log
+truncate -s0 ~/.bitgreen_btg4/debug.log
+truncate -s0 ~/.bitgreen_btg5/debug.log
+truncate -s0 ~/.bitgreen_btg6/debug.log
+truncate -s0 ~/.bitgreen_btg7/debug.log
+truncate -s0 ~/.bitgreen_btg8/debug.log
+truncate -s0 ~/.bitgreen_btg9/debug.log
+truncate -s0 ~/.bitgreen_btg10/debug.log
+sleep 5
+truncate -s0 ~/.cazcoin/debug.log
+truncate -s0 ~/.cazcoin2/debug.log
+truncate -s0 ~/.cazcoin3/debug.log
+sleep 5
+truncate -s0 ~/.GainerCoin/debug.log
+sleep 5
+truncate -s0 ~/.genixcore/debug.log
+truncate -s0 ~/.genixcore2/debug.log
+truncate -s0 ~/.genixcore3/debug.log
+truncate -s0 ~/.genixcore4/debug.log
+truncate -s0 ~/.genixcore5/debug.log
+truncate -s0 ~/.genixcore6/debug.log
+sleep 5
+truncate -s0 ~/.ghrcoin/debug.log
+truncate -s0 ~/.ghrcoin2/debug.log
+truncate -s0 ~/.ghrcoin3/debug.log
+sleep 5
+truncate -s0 ~/.innovacore/debug.log
+truncate -s0 ~/.innovacore2/debug.log
+truncate -s0 ~/.innovacore3/debug.log
+truncate -s0 ~/.innovacore4/debug.log
+truncate -s0 ~/.innovacore5/debug.log
+truncate -s0 ~/.innovacore6/debug.log
+truncate -s0 ~/.innovacore7/debug.log
+truncate -s0 ~/.innovacore8/debug.log
+truncate -s0 ~/.innovacore9/debug.log
+truncate -s0 ~/.innovacore10/debug.log
+truncate -s0 ~/.innovacore11/debug.log
+truncate -s0 ~/.innovacore12/debug.log
+truncate -s0 ~/.innovacore13/debug.log
+truncate -s0 ~/.innovacore14/debug.log
+truncate -s0 ~/.innovacore15/debug.log
+truncate -s0 ~/.innovacore16/debug.log
+truncate -s0 ~/.innovacore17/debug.log
+truncate -s0 ~/.innovacore18/debug.log
+truncate -s0 ~/.innovacore19/debug.log
+truncate -s0 ~/.innovacore20/debug.log
+sleep 5
+truncate -s0 ~/.jemcash/debug.log
+truncate -s0 ~/.jemcash2/debug.log
+truncate -s0 ~/.jemcash3/debug.log
+truncate -s0 ~/.jemcash4/debug.log
+truncate -s0 ~/.jemcash5/debug.log
+truncate -s0 ~/.jemcash6/debug.log
+sleep 5
+truncate -s0 ~/.konjungatecore/debug.log
+truncate -s0 ~/.konjungatecore2/debug.log
+truncate -s0 ~/.konjungatecore3/debug.log
+truncate -s0 ~/.konjungatecore4/debug.log
+truncate -s0 ~/.konjungatecore5/debug.log
+truncate -s0 ~/.konjungatecore6/debug.log
+truncate -s0 ~/.konjungatecore7/debug.log
+truncate -s0 ~/.konjungatecore8/debug.log
+truncate -s0 ~/.konjungatecore9/debug.log
+truncate -s0 ~/.konjungatecore10/debug.log
+truncate -s0 ~/.konjungatecore11/debug.log
+truncate -s0 ~/.konjungatecore12/debug.log
+truncate -s0 ~/.konjungatecore13/debug.log
+sleep 5
+truncate -s0 ~/.metrix7/debug.log
+truncate -s0 ~/.metrix8/debug.log
+truncate -s0 ~/.metrix9/debug.log
+truncate -s0 ~/.metrix10/debug.log
+truncate -s0 ~/.metrix11/debug.log
+truncate -s0 ~/.metrix12/debug.log
+truncate -s0 ~/.metrix13/debug.log
+truncate -s0 ~/.metrix14/debug.log
+truncate -s0 ~/.metrix15/debug.log
+truncate -s0 ~/.metrix16/debug.log
+truncate -s0 ~/.metrix17/debug.log
+truncate -s0 ~/.metrix18/debug.log
+truncate -s0 ~/.metrix19/debug.log
+truncate -s0 ~/.metrix20/debug.log
+truncate -s0 ~/.metrix21/debug.log
+truncate -s0 ~/.metrix22/debug.log
+truncate -s0 ~/.metrix23/debug.log
+truncate -s0 ~/.metrix24/debug.log
+truncate -s0 ~/.metrix25/debug.log
+truncate -s0 ~/.metrix26/debug.log
+truncate -s0 ~/.metrix27/debug.log
+truncate -s0 ~/.metrix28/debug.log
+truncate -s0 ~/.metrix29/debug.log
+truncate -s0 ~/.metrix30/debug.log
+sleep 5
+truncate -s0 ~/.mktcoin/debug.log
+truncate -s0 ~/.mktcoin2/debug.log
+truncate -s0 ~/.mktcoin3/debug.log
+truncate -s0 ~/.mktcoin4/debug.log
+truncate -s0 ~/.mktcoin5/debug.log
+truncate -s0 ~/.mktcoin6/debug.log
+truncate -s0 ~/.mktcoin7/debug.log
+truncate -s0 ~/.mktcoin8/debug.log
+truncate -s0 ~/.mktcoin9/debug.log
+truncate -s0 ~/.mktcoin10/debug.log
+truncate -s0 ~/.mktcoin11/debug.log
+truncate -s0 ~/.mktcoin12/debug.log
+truncate -s0 ~/.mktcoin13/debug.log
+truncate -s0 ~/.mktcoin14/debug.log
+truncate -s0 ~/.mktcoin15/debug.log
+truncate -s0 ~/.mktcoin16/debug.log
+truncate -s0 ~/.mktcoin17/debug.log
+truncate -s0 ~/.mktcoin18/debug.log
+truncate -s0 ~/.mktcoin19/debug.log
+truncate -s0 ~/.mktcoin20/debug.log
+truncate -s0 ~/.mktcoin21/debug.log
+truncate -s0 ~/.mktcoin22/debug.log
+truncate -s0 ~/.mktcoin23/debug.log
+sleep 5
+truncate -s0 ~/.MoneyByte/debug.log
+sleep 5
+truncate -s0 ~/.monkey_161/debug.log
+truncate -s0 ~/.monkey_162/debug.log
+truncate -s0 ~/.monkey_163/debug.log
+truncate -s0 ~/.monkey_164/debug.log
+truncate -s0 ~/.monkey_liones11/debug.log
+truncate -s0 ~/.monkey_liones12/debug.log
+truncate -s0 ~/.monkey_liones81/debug.log
+truncate -s0 ~/.monkey_liones131/debug.log
+truncate -s0 ~/.monkey_liones132/debug.log
+truncate -s0 ~/.monkey_liones133/debug.log
+truncate -s0 ~/.monkey_liones134/debug.log
+truncate -s0 ~/.monkey_liones135/debug.log
+truncate -s0 ~/.monkey_wave1/debug.log
+truncate -s0 ~/.monkey_wave2/debug.log
+truncate -s0 ~/.monkey_wave3/debug.log
+truncate -s0 ~/.monkey_wave4/debug.log
+truncate -s0 ~/.monkey_wave5/debug.log
+truncate -s0 ~/.monkey_wave6/debug.log
+truncate -s0 ~/.monkey_wave7/debug.log
+truncate -s0 ~/.monkey_wave8/debug.log
+truncate -s0 ~/.monkey_wave9/debug.log
+truncate -s0 ~/.monkey_wave10/debug.log
+truncate -s0 ~/.monkey_wave11/debug.log
+truncate -s0 ~/.monkey_wave12/debug.log
+truncate -s0 ~/.monkey_wave13/debug.log
+truncate -s0 ~/.monkey_wave14/debug.log
+truncate -s0 ~/.monkey_wave15/debug.log
+sleep 5
+truncate -s0 ~/.rupayacore/debug.log
+truncate -s0 ~/.rupayacore2/debug.log
+truncate -s0 ~/.rupayacore3/debug.log
+truncate -s0 ~/.rupayacore4/debug.log
+truncate -s0 ~/.rupayacore5/debug.log
+truncate -s0 ~/.rupayacore6/debug.log
+sleep 5
+truncate -s0 ~/.stashcore/debug.log
+truncate -s0 ~/.stashcore2/debug.log
+truncate -s0 ~/.stashcore3/debug.log
+truncate -s0 ~/.stashcore4/debug.log
+truncate -s0 ~/.stashcore5/debug.log
+truncate -s0 ~/.stashcore6/debug.log
+truncate -s0 ~/.stashcore7/debug.log
+truncate -s0 ~/.stashcore8/debug.log
+truncate -s0 ~/.stashcore9/debug.log
+truncate -s0 ~/.stashcore10/debug.log
+truncate -s0 ~/.stashcore11/debug.log
+sleep 5
+truncate -s0 ~/.tokugawacore/debug.log
+truncate -s0 ~/.tokugawacore2/debug.log
+truncate -s0 ~/.tokugawacore3/debug.log
+sleep 5
+truncate -s0 ~/.vivocore/debug.log
+sleep 5
+truncate -s0 ~/.xsncore/debug.log
+truncate -s0 ~/.xsncore2/debug.log
