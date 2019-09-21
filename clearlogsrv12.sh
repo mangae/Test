de855d386e7253b191f312d939663bfc9c6da78f
mangae <37798589+mangae@users.noreply.github.com>
2019-09-21 18:08:22 +0200

    Update and rename clearlogsrv11.sh to clearlogsrv12.sh

---
+++ clearlogsrv12.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+truncate -s0 ~/.mktcoin/debug.log
+sleep 5
+truncate -s0 ~/.stashcore/debug.log
+truncate -s0 ~/.stashcore2/debug.log
+truncate -s0 ~/.stashcore3/debug.log
+truncate -s0 ~/.stashcore4/debug.log
+truncate -s0 ~/.stashcore5/debug.log
+truncate -s0 ~/.stashcore6/debug.log
+truncate -s0 ~/.stashcore7/debug.log
+truncate -s0 ~/.stashcore8/debug.log
+truncate -s0 ~/.stashcore9/debug.log
+truncate -s0 ~/.stashcore10/debug.log
+truncate -s0 ~/.stashcore11/debug.log
