bd57aa94b337abedc68155b0e92e60eb1092b34e
mangae <37798589+mangae@users.noreply.github.com>
2019-09-21 14:29:52 +0200

    Update and rename clearlogsrv4.sh to clearlogsrv5.sh

---
+++ clearlogsrv5.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+truncate -s0 ~/.Konjungate/debug.log
+truncate -s0 ~/.Konjungate2/debug.log
+truncate -s0 ~/.Konjungate3/debug.log
+truncate -s0 ~/.Konjungate4/debug.log
+truncate -s0 ~/.Konjungate5/debug.log
+sleep 5
+truncate -s0 ~/.konjungatecore/debug.log
+truncate -s0 ~/.konjungatecore2/debug.log
+truncate -s0 ~/.konjungatecore3/debug.log
+truncate -s0 ~/.konjungatecore4/debug.log
+truncate -s0 ~/.konjungatecore5/debug.log
+truncate -s0 ~/.konjungatecore6/debug.log
+truncate -s0 ~/.konjungatecore7/debug.log
+truncate -s0 ~/.konjungatecore8/debug.log
+truncate -s0 ~/.konjungatecore9/debug.log
+truncate -s0 ~/.konjungatecore10/debug.log
+sleep 5
+truncate -s0 ~/.metrix/debug.log
+truncate -s0 ~/.metrix2/debug.log
+truncate -s0 ~/.metrix3/debug.log
+truncate -s0 ~/.metrix4/debug.log
+truncate -s0 ~/.metrix5/debug.log
+truncate -s0 ~/.metrix6/debug.log
+truncate -s0 ~/.metrix7/debug.log
+truncate -s0 ~/.metrix8/debug.log
+truncate -s0 ~/.metrix9/debug.log
+truncate -s0 ~/.metrix10/debug.log
+truncate -s0 ~/.metrix11/debug.log
+truncate -s0 ~/.metrix12/debug.log
+sleep 5
+truncate -s0 ~/.mktcoin/debug.log
+truncate -s0 ~/.mktcoin2/debug.log
+truncate -s0 ~/.mktcoin3/debug.log
+truncate -s0 ~/.mktcoin4/debug.log
+truncate -s0 ~/.mktcoin5/debug.log
+truncate -s0 ~/.mktcoin6/debug.log
+truncate -s0 ~/.mktcoin7/debug.log
+truncate -s0 ~/.mktcoin8/debug.log
+truncate -s0 ~/.mktcoin9/debug.log
+truncate -s0 ~/.mktcoin10/debug.log
+truncate -s0 ~/.mktcoin11/debug.log
+truncate -s0 ~/.mktcoin12/debug.log
+truncate -s0 ~/.mktcoin13/debug.log
+truncate -s0 ~/.mktcoin14/debug.log
+truncate -s0 ~/.mktcoin15/debug.log
+truncate -s0 ~/.mktcoin16/debug.log
+truncate -s0 ~/.mktcoin17/debug.log
+truncate -s0 ~/.mktcoin18/debug.log
+truncate -s0 ~/.mktcoin19/debug.log
+truncate -s0 ~/.mktcoin20/debug.log
+truncate -s0 ~/.mktcoin21/debug.log
+sleep 5
+truncate -s0 ~/.MoneyByte/debug.log
