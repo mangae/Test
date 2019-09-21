b295199269a677fd6679c33432406901ed9a3c74
mangae <37798589+mangae@users.noreply.github.com>
2019-09-21 18:04:56 +0200

    Update and rename clearlogsrv10.sh to clearlogsrv11.sh

---
+++ clearlogsrv11.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+truncate -s0 ~/.metrix/debug.log
+truncate -s0 ~/.metrix2/debug.log
+truncate -s0 ~/.metrix3/debug.log
+truncate -s0 ~/.metrix4/debug.log
+truncate -s0 ~/.metrix5/debug.log
+truncate -s0 ~/.metrix6/debug.log
+truncate -s0 ~/.metrix7/debug.log
+truncate -s0 ~/.metrix8/debug.log
+truncate -s0 ~/.metrix9/debug.log
+truncate -s0 ~/.metrix10/debug.log
+truncate -s0 ~/.metrix11/debug.log
+sleep 5
+truncate -s0 ~/.mktcoin/debug.log
+truncate -s0 ~/.mktcoin2/debug.log
+truncate -s0 ~/.mktcoin3/debug.log
+truncate -s0 ~/.mktcoin4/debug.log
+truncate -s0 ~/.mktcoin5/debug.log
+truncate -s0 ~/.mktcoin6/debug.log
+truncate -s0 ~/.mktcoin7/debug.log
+truncate -s0 ~/.mktcoin8/debug.log
+truncate -s0 ~/.mktcoin9/debug.log
+truncate -s0 ~/.mktcoin10/debug.log
+truncate -s0 ~/.mktcoin11/debug.log
+truncate -s0 ~/.mktcoin12/debug.log
+truncate -s0 ~/.mktcoin13/debug.log
+truncate -s0 ~/.mktcoin14/debug.log
+truncate -s0 ~/.mktcoin15/debug.log
+truncate -s0 ~/.mktcoin16/debug.log
+truncate -s0 ~/.mktcoin17/debug.log
+truncate -s0 ~/.mktcoin18/debug.log
+truncate -s0 ~/.mktcoin19/debug.log
+truncate -s0 ~/.mktcoin20/debug.log
+sleep 5
+truncate -s0 ~/.MoneyByte/debug.log
+sleep 5
+truncate -s0 ~/.rupayacore7/debug.log
+truncate -s0 ~/.rupayacore8/debug.log
+truncate -s0 ~/.rupayacore9/debug.log
+truncate -s0 ~/.rupayacore10/debug.log
+sleep 5
+truncate -s0 ~/.stashcore/debug.log
+truncate -s0 ~/.stashcore2/debug.log
+truncate -s0 ~/.stashcore3/debug.log
+truncate -s0 ~/.stashcore4/debug.log
+truncate -s0 ~/.stashcore5/debug.log
+truncate -s0 ~/.stashcore6/debug.log
+truncate -s0 ~/.stashcore7/debug.log
+truncate -s0 ~/.stashcore8/debug.log
+truncate -s0 ~/.stashcore9/debug.log
+truncate -s0 ~/.stashcore10/debug.log
+truncate -s0 ~/.stashcore11/debug.log
